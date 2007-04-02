ee636bf9734363a179bc4fe277d4edfb522f81d2
bergen <bergen@6bc4af11-be57-4202-972a-8b33214847af>
2007-04-02 21:10:01 +0000

    adding scalar machine file (no SIMD)

--- src/util/v4/v4.hxx
+++ src/util/v4/v4.hxx
@@ -1,13 +1,15 @@
 #ifndef v4_hxx
 #define v4_hxx
 
-#if defined __SSE__
+/* pick the appropriate ISA specific implementation */
+#if defined SCALAR
+	#include <v4_portable.hxx>
+#elif defined __SSE__
 	#include <v4_sse.hxx>
 #elif defined __ALTIVEC__
+	#include <v4_altivec.hxx>
 #elif defined __SPU__
 	#include <v4_spu.hxx>
-#else
-	#include <v4_portable.hxx>
 #endif // ISA
 
 #endif // v4_hxx

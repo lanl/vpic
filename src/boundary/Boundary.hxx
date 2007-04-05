acf1be44ac507529b8450b049428f241f1ef0627
bergen <bergen@6bc4af11-be57-4202-972a-8b33214847af>
2007-04-05 14:42:20 +0000

    testing file information

---
+++ src/boundary/Boundary.hxx
@@ -0,0 +1,41 @@
+/*
+	Definition of Boundary_T class
+
+	$Revision$
+	$Date$
+	$Author$
+
+	vim: set ts=3 :
+*/
+#ifndef Boundary_h
+#define Boundary_h
+
+/*!
+	\class Boundary Boundary.h
+	\brief Boundary handler class.
+*/
+class Boundary_T
+	{
+	public:
+
+		//! Constructor
+		Boundary_T();
+
+		//! Destructor
+		~Boundary_T();
+
+	private:
+
+	}; // class Boundary_T
+
+typedef Boundary_T Boundary;
+
+Boundary_T::Boundary_T()
+	{
+	} // Boundary_T::Boundary_T
+
+Boundary_T::~Boundary_T()
+	{
+	} // Boundary_T::~Boundary_T
+
+#endif // Boundary_h

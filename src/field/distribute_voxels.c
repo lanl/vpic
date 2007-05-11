2f2461d78b0ca36a41dfb4f60a11395b5719728a
kbowers <kbowers@6bc4af11-be57-4202-972a-8b33214847af>
2007-05-11 12:55:48 +0000

    Fixed a divide-by-zero bug when computing the index of first bulk voxel to process in distribute_voxels.  This bug would trigger when the local voxel mesh was so small that the bulk voxel region used for pipelines contained no voxels (e.g. 1d and probably some 2d simulations).
    Note that even in the presence of the divide-by-zero, the number of voxels
    assigned to each pipeline was still correct.  Thus, technically, this bug
    could not affect computations (i.e. a pipeline would have been told to process
    zero voxels starting from an invalid voxel index).  However, some platforms
    (presumably Cell) are not forgiving to divide-by-zeros and fault when they
    occur.  So the voxel index computation is now well defined with no
    divide-by-zeros for any region parameters.
    
    Hat tip to Ben for finding the bug.
    
    (Staggered meshes are fun to efficiently index.)

--- src/field/distribute_voxels.c
+++ src/field/distribute_voxels.c
@@ -6,13 +6,13 @@ distribute_voxels( int x0, int x1,     // range of x-indices (inclusive)
                    int z0, int z1,     // range of z-indices (inclusive)
                    int job, int n_job, // job ... on [0,n_job-1]
                    int * _x, int * _y, int * _z ) {
-  const int nvx = x1-x0+1;
-  const int nvy = y1-y0+1;
-  const int nvz = z1-z0+1;
+  const int nvx = x1-x0+1; // Number of voxels along x to process
+  const int nvy = y1-y0+1; // Number of voxels along y to process
+  const int nvz = z1-z0+1; // Number of voxels along z to process
 
   int x, y, z, n_voxel;
 
-  n_voxel = nvx*nvy*nvz;
+  n_voxel = nvx*nvy*nvz; // Number of voxels to process
 
   if( job==n_job ) {
 
@@ -20,7 +20,7 @@ distribute_voxels( int x0, int x1,     // range of x-indices (inclusive)
 
     x  = n_voxel;
     n_voxel &= 3;
-    x -= n_voxel;
+    x -= n_voxel; 
 
   } else {
 
@@ -32,11 +32,11 @@ distribute_voxels( int x0, int x1,     // range of x-indices (inclusive)
 
   }
 
-  /**/           // x = (x-x0) + nvx*( (y-y0) + nvy*(z-z0) )
-  y  = x/nvx;    // y =                (y-y0) + nvy*(z-z0)
-  z  = y/nvy;    // z =                             (z-z0)
-  x -= y*nvx;    // x = (x-x0)
-  y -= z*nvy;    // y =                (y-y0)
+  /**/                     // x = (x-x0) + nvx*( (y-y0) + nvy*(z-z0) )
+  y  = nvx==0 ? 0 : x/nvx; // y =                (y-y0) + nvy*(z-z0)
+  z  = nvy==0 ? 0 : y/nvy; // z =                             (z-z0)
+  x -= y*nvx;              // x = (x-x0)
+  y -= z*nvy;              // y =                (y-y0)
 
   x += x0;
   y += y0;

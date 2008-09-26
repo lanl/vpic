ab7e2c89d0a23d6e601f4b5a725ca9aed499922c
bergen <bergen@6bc4af11-be57-4202-972a-8b33214847af>
2008-09-26 05:18:15 +0000

    Fixed a couple of build issues to do with library name changes. Thank you IBM...

--- src/util/relay/Type2DaCSSwapType.hxx
+++ src/util/relay/Type2DaCSSwapType.hxx
@@ -17,6 +17,12 @@ template<> struct Type2DaCSSwapType<int>
 			{ return DACS_BYTE_SWAP_WORD; }
 	}; // struct Type2DaCSSwapType
 
+template<> struct Type2DaCSSwapType<uint32_t>
+	{
+		inline static DACS_BYTE_SWAP_T type()
+			{ return DACS_BYTE_SWAP_WORD; }
+	}; // struct Type2DaCSSwapType
+
 template<> struct Type2DaCSSwapType<int64_t>
 	{
 		inline static DACS_BYTE_SWAP_T type()

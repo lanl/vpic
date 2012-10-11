8093e30751a0cee48e30ed02bd7d9e811a174ede
Ben Bergen <bergen@lanl.gov>
2012-10-11 14:05:05 -0600

    Fixed absolute path in distribution.

--- test/unit/unit.cxx
+++ test/unit/unit.cxx
@@ -2,16 +2,19 @@
 
 void vpic_simulation::user_initialization(int num_cmdline_arguments,
 	char ** cmdline_argument) {
-}
+} // vpic_simulation::user_initialization
 
 void vpic_simulation::user_diagnostics() {
-}
+} // vpic_simulation::user_diagnostics
 
 void vpic_simulation::user_particle_injection() {
-}
+} // vpic_simulation::user_particle_injection
 
 void vpic_simulation::user_current_injection() {
-}
+} // vpic_simulation::user_current_injection
 
 void vpic_simulation::user_field_injection() {
-}
+} // vpic_simulation::user_field_injection
+
+void vpic_simulation::user_particle_collisions() {
+} // vpic_simulation::user_particle_collisions

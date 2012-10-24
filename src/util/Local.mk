617d1cd14e43101dc38c805bbee62203595b4fd7
Ben Bergen <bergen@lanl.gov>
2012-10-24 13:41:36 -0600

    Updating and renaming.
    Removed old overlay directory.  Renamed CheckPtFileIO to be more consistent
    with nameing style.

--- src/util/Local.mk
+++ src/util/Local.mk
@@ -2,4 +2,4 @@ $(call make-lib,vpicutil)
 $(call add-hdrs,util_base.h util.h)
 $(call add-objs,util_base boot,vpicutil)
 # FIXME: EXPOSE THESE TO INPUT DECKS?
-$(call add-hdrs,BitField.hxx CheckSum.hxx SystemRAM.hxx)
+$(call add-hdrs,bitfield.hxx checksum.hxx system.hxx)

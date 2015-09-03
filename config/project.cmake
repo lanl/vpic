b5f447f76c2c712c642920595911f52172750731
John Wohlbier <wohlbier@lanl.gov>
2015-09-03 15:31:18 -0600

    add V4 build options to project.cmake. In v4.h change header file names from .hxx to .h.
    Verified USE_V4_SSE builds.
    Can't test USE_V4_ALTIVEC on my platforms.
    USE_V4_PORTABLE fails with the following output with intel compiler.
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(56): error: no operator "*" matches these operands
                operand types are: v4::v4float * v4::v4 (const v4::v4 &, int)
        q  = v4float(qsp)*splat<3>(u); // q  = p_q,   p_q,   p_q,   D/C
                         ^
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(58): error: cannot determine which instance of overloaded function "shuffle" is intended
        dr = shuffle<0,1,2,2>( dr );  // dr = p_ddx, p_ddy, p_ddz, D/C
             ^
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(59): error: cannot determine which instance of overloaded function "shuffle" is intended
        r  = shuffle<0,1,2,2>( r );  // r  = p_dx,  p_dy,  p_dz,  D/C
             ^
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(119): error: no operator "*" matches these operands
                operand types are: v4::v4float * <unknown-type>
          v1  = v4*shuffle<1,2,0,3>( v5 );
                  ^
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(124): error: cannot determine which instance of overloaded function "shuffle" is intended
          v5  = shuffle<2,0,1,3>( v5 ); // v5 = dz, dx, dy, D/C
                ^
    
    /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc(133): error: no operator "*" matches these operands
                operand types are: v4::v4float * v4::v4 (const v4::v4 &, int)
          v4  = ((q3*splat<0>(sdr))*splat<1>(sdr))*splat<2>(sdr);
                    ^
    
    compilation aborted for /home/wohlbier/VPIC/vpic/src/species_advance/standard/move_p.cc (code 2)
    make[2]: *** [CMakeFiles/vpic.dir/src/species_advance/standard/move_p.cc.o] Error 2
    make[1]: *** [CMakeFiles/vpic.dir/all] Error 2
    make: *** [all] Error 2

--- config/project.cmake
+++ config/project.cmake
@@ -12,6 +12,24 @@ set(ENABLE_MPI True CACHE BOOL "Enable MPI" FORCE)
 set(ENABLE_MPI_CXX_BINDINGS False CACHE BOOL "Enable MPI C++ Bindings" FORCE)
 
 #------------------------------------------------------------------------------#
+# Add build options
+#------------------------------------------------------------------------------#
+option(USE_V4_ALTIVEC "Enable V4 Altivec" OFF)
+if(USE_V4_ALTIVEC)
+  add_definitions(-DUSE_V4_ALTIVEC)
+endif(USE_V4_ALTIVEC)
+
+option(USE_V4_PORTABLE "Enable V4 Portable" OFF)
+if(USE_V4_PORTABLE)
+  add_definitions(-DUSE_V4_PORTABLE)
+endif(USE_V4_PORTABLE)
+
+option(USE_V4_SSE "Enable V4 SSE" OFF)
+if(USE_V4_SSE)
+  add_definitions(-DUSE_V4_SSE)
+endif(USE_V4_SSE)
+
+#------------------------------------------------------------------------------#
 # Add library target
 #------------------------------------------------------------------------------#
 
--- src/util/v4/v4.h
+++ src/util/v4/v4.h
@@ -4,12 +4,12 @@
 #define IN_v4_h
 /* FIXME: SHOULDN'T THIS INCLUDE UTIL_BASE.H? */
 #ifdef __cplusplus
-# if defined USE_V4_PORTABLE
-#   include "v4_portable.hxx"
+# if defined USE_V4_ALTIVEC
+#   include "v4_altivec.h"
+# elif defined USE_V4_PORTABLE
+#   include "v4_portable.h"
 # elif defined USE_V4_SSE
-#   include "v4_sse.hxx"
-# elif defined USE_V4_ALTIVEC
-#   include "v4_altivec.hxx"
+#   include "v4_sse.h"
 # endif
 #endif
 #undef IN_v4_h

385b2389af346fb087ce3e6c647c52fa20559561
Chet Nieter <chet.nieter@kitware.com>
2016-09-15 14:07:43 -0400

    New scatterplot script.
    Created a new scatterplot script since the old one was given errors
    from the rendering code on taanab. Maybe due to the recent changes
    to paraview/catalyst to deal with the render window bug.

--- scripts/catalyst/scatterplot.py
+++ scripts/catalyst/scatterplot.py
@@ -5,7 +5,7 @@ from paraview import coprocessing
 
 #--------------------------------------------------------------
 # Code generated from cpstate.py to create the CoProcessor.
-# ParaView 5.1.2 64 bits
+# ParaView 5.1.2-399-g6324cb6 64 bits
 
 
 # ----------------------- CoProcessor definition -----------------------
@@ -13,7 +13,7 @@ from paraview import coprocessing
 def CreateCoProcessor():
   def _CreatePipeline(coprocessor, datadescription):
     class Pipeline:
-      # state file generated using paraview version 5.1.2
+      # state file generated using paraview version 5.1.2-399-g6324cb6
 
       # ----------------------------------------------------------------
       # setup views used in the visualization
@@ -24,21 +24,21 @@ def CreateCoProcessor():
 
       # Create a new 'Render View'
       renderView1 = CreateView('RenderView')
-      renderView1.ViewSize = [791, 512]
+      renderView1.ViewSize = [1174, 929]
       renderView1.AxesGrid = 'GridAxes3DActor'
-      renderView1.CenterOfRotation = [15.234381385147572, -6.9141387939453125e-06, 0.23440146446228027]
+      renderView1.CenterOfRotation = [15.234395240433514, -4.76837158203125e-07, 0.11717867851257324]
       renderView1.StereoType = 0
-      renderView1.CameraPosition = [10.644077829285354, 86.09566389824528, -18.05071227374809]
-      renderView1.CameraFocalPoint = [15.234381385147582, -6.9141387839099245e-06, 0.23440146446226215]
-      renderView1.CameraViewUp = [0.9801362825393347, 0.010192348917000493, -0.198063080036456]
-      renderView1.CameraParallelScale = 18.852204262064895
+      renderView1.CameraPosition = [17.27228783074267, 71.63250368971187, -9.285649562306032]
+      renderView1.CameraFocalPoint = [15.234395240433512, -4.768371590034197e-07, 0.11717867851257469]
+      renderView1.CameraViewUp = [-0.8792420791071403, -0.03732527606443192, -0.474910718023996]
+      renderView1.CameraParallelScale = 18.706336660411555
       renderView1.Background = [0.32, 0.34, 0.43]
 
       # register the view with coprocessor
       # and provide it with information such as the filename to use,
       # how frequently to write the images, etc.
       coprocessor.RegisterView(renderView1,
-          filename='electron_scatterplot_%t.png', freq=1, fittoscreen=0, magnification=1, width=791, height=512, cinema={})
+          filename='electron_scatterplot_%t.png', freq=15, fittoscreen=0, magnification=1, width=1174, height=929, cinema={})
       renderView1.ViewTime = datadescription.GetTime()
 
       # ----------------------------------------------------------------
@@ -51,37 +51,38 @@ def CreateCoProcessor():
 
       # create a new 'Glyph'
       glyph1 = Glyph(Input=electron,
-          GlyphType='Sphere')
+          GlyphType='Arrow')
       glyph1.Scalars = ['POINTS', 'None']
       glyph1.Vectors = ['POINTS', 'momentum']
-      glyph1.ScaleFactor = 3.0468695268034938
-      glyph1.MaximumNumberOfSamplePoints = 50000
+      glyph1.ScaleMode = 'scalar'
+      glyph1.ScaleFactor = 3.046867900155485
+      glyph1.MaximumNumberOfSamplePoints = 30000
+      glyph1.Stride = 200
       glyph1.GlyphTransform = 'Transform2'
-
-      # init the 'Sphere' selected for 'GlyphType'
-      glyph1.GlyphType.Radius = 0.05
-
-      # create a new 'Parallel PolyData Writer'
-      parallelPolyDataWriter1 = servermanager.writers.XMLPPolyDataWriter(Input=glyph1)
-
-      # register the writer with coprocessor
-      # and provide it with information such as the filename to use,
-      # how frequently to write the data, etc.
-      coprocessor.RegisterWriter(parallelPolyDataWriter1, filename='filename_%t.pvtp', freq=1)
 
       # ----------------------------------------------------------------
       # setup color maps and opacity mapes used in the visualization
       # note: the Get..() functions create a new object, if needed
       # ----------------------------------------------------------------
 
+      # get color transfer function/color map for 'weights'
+      weightsLUT = GetColorTransferFunction('weights')
+      weightsLUT.RGBPoints = [0.002574920654296875, 0.231373, 0.298039, 0.752941, 0.002582550048828125, 0.865003, 0.865003, 0.865003, 0.002590179443359375, 0.705882, 0.0156863, 0.14902]
+      weightsLUT.ScalarRangeInitialized = 1.0
+
+      # get opacity transfer function/opacity map for 'weights'
+      weightsPWF = GetOpacityTransferFunction('weights')
+      weightsPWF.Points = [0.002574920654296875, 0.0, 0.5, 0.0, 0.002590179443359375, 1.0, 0.5, 0.0]
+      weightsPWF.ScalarRangeInitialized = 1
+
       # get color transfer function/color map for 'GlyphVector'
       glyphVectorLUT = GetColorTransferFunction('GlyphVector')
-      glyphVectorLUT.RGBPoints = [0.00900907972479836, 0.231373, 0.298039, 0.752941, 0.5441682644121746, 0.865003, 0.865003, 0.865003, 1.0793274490995508, 0.705882, 0.0156863, 0.14902]
+      glyphVectorLUT.RGBPoints = [0.009482396371128719, 0.231373, 0.298039, 0.752941, 0.5585663030005522, 0.865003, 0.865003, 0.865003, 1.1076502096299758, 0.705882, 0.0156863, 0.14902]
       glyphVectorLUT.ScalarRangeInitialized = 1.0
 
       # get opacity transfer function/opacity map for 'GlyphVector'
       glyphVectorPWF = GetOpacityTransferFunction('GlyphVector')
-      glyphVectorPWF.Points = [0.00900907972479836, 0.0, 0.5, 0.0, 1.0793274490995508, 1.0, 0.5, 0.0]
+      glyphVectorPWF.Points = [0.009482396371128719, 0.0, 0.5, 0.0, 1.1076502096299758, 1.0, 0.5, 0.0]
       glyphVectorPWF.ScalarRangeInitialized = 1
 
       # ----------------------------------------------------------------
@@ -91,41 +92,52 @@ def CreateCoProcessor():
       # show data from electron
       electronDisplay = Show(electron, renderView1)
       # trace defaults for the display properties.
-      electronDisplay.ColorArrayName = [None, '']
-      electronDisplay.OSPRayScaleArray = 'momentum'
+      electronDisplay.ColorArrayName = ['POINTS', 'weights']
+      electronDisplay.LookupTable = weightsLUT
+      electronDisplay.OSPRayScaleArray = 'weights'
       electronDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+      electronDisplay.SelectOrientationVectors = 'momentum'
+      electronDisplay.ScaleFactor = 3.046867900155485
+      electronDisplay.SelectScaleArray = 'weights'
       electronDisplay.GlyphType = 'Arrow'
-      electronDisplay.SetScaleArray = [None, '']
-      electronDisplay.ScaleTransferFunction = 'PiecewiseFunction'
-      electronDisplay.OpacityArray = [None, '']
-      electronDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+
+      # show color legend
+      electronDisplay.SetScalarBarVisibility(renderView1, True)
 
       # show data from glyph1
       glyph1Display = Show(glyph1, renderView1)
       # trace defaults for the display properties.
+      glyph1Display.Representation = 'Points'
       glyph1Display.ColorArrayName = ['POINTS', 'GlyphVector']
       glyph1Display.LookupTable = glyphVectorLUT
-      glyph1Display.OSPRayScaleArray = 'GlyphVector'
+      glyph1Display.Opacity = 0.74
+      glyph1Display.OSPRayScaleArray = 'weights'
       glyph1Display.OSPRayScaleFunction = 'PiecewiseFunction'
-      glyph1Display.GlyphType = 'Arrow'
-      glyph1Display.SetScaleArray = [None, '']
-      glyph1Display.ScaleTransferFunction = 'PiecewiseFunction'
-      glyph1Display.OpacityArray = [None, '']
-      glyph1Display.OpacityTransferFunction = 'PiecewiseFunction'
+      glyph1Display.SelectOrientationVectors = 'GlyphVector'
+      glyph1Display.ScaleFactor = 3.349505639076233
+      glyph1Display.SelectScaleArray = 'weights'
+      glyph1Display.GlyphType = 'Sphere'
 
       # show color legend
       glyph1Display.SetScalarBarVisibility(renderView1, True)
 
       # setup the color legend parameters for each legend in this view
 
+      # get color legend/bar for weightsLUT in view renderView1
+      weightsLUTColorBar = GetScalarBar(weightsLUT, renderView1)
+      weightsLUTColorBar.Title = 'weights'
+      weightsLUTColorBar.ComponentTitle = ''
+
       # get color legend/bar for glyphVectorLUT in view renderView1
       glyphVectorLUTColorBar = GetScalarBar(glyphVectorLUT, renderView1)
+      glyphVectorLUTColorBar.Position = [0.85, 0.52]
+      glyphVectorLUTColorBar.Position2 = [0.12, 0.42999999999999994]
       glyphVectorLUTColorBar.Title = 'GlyphVector'
       glyphVectorLUTColorBar.ComponentTitle = 'Magnitude'
 
       # ----------------------------------------------------------------
       # finally, restore active source
-      SetActiveSource(parallelPolyDataWriter1)
+      SetActiveSource(glyph1)
       # ----------------------------------------------------------------
     return Pipeline()
 
@@ -135,7 +147,7 @@ def CreateCoProcessor():
 
   coprocessor = CoProcessor()
   # these are the frequencies at which the coprocessor updates.
-  freqs = {'electron': [5]}
+  freqs = {'electron': [15, 15]}
   coprocessor.SetUpdateFrequencies(freqs)
   return coprocessor
 

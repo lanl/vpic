14634ebaa519a50e4ab132a8c0a1d730d24e51d6
Robert Bird <bird@lanl.gov>
2019-01-16 13:28:21 -0700

    split function up into a couple of methods, and added code to log errors to file for further anaylsis

--- test/unit/energy_comparison/compare_energies.h
+++ test/unit/energy_comparison/compare_energies.h
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <limits>
+
+#include <limits> // epsilon for limit
+#include <utility> // pair
+
+// TODO: add a namespace or similar
 
 /**
- * @brief  TODO
+ * @brief Helper function to write collective errors to file for further analysis
  *
- * @param errs
- * @param field_per_line
+ * @param errs The vector of all errors
+ * @param field_per_line The number of values to write per file line
  */
 void write_error_ouput( std::vector<double> errs, int field_per_line)
 {
@@ -28,22 +32,100 @@ void write_error_ouput( std::vector<double> errs, int field_per_line)
 }
 
 /**
+ * @brief Helper function to compare numbers and calculate a absolute error
+ *
+ * @param A The first value to compare
+ * @param B The second value to compare
+ *
+ * @return The calculated error
+ */
+double calculate_abs_error(double A, double B)
+{
+    return std::abs(A-B) / std::min(A,B);
+}
+
+/**
+ * @brief Helper function to compare numbers and calculate a relative error
+ *
+ * @param A The first value to compare
+ * @param B The second value to compare
+ *
+ * @return The calculated error
+ */
+double calculate_relative_error(double A, double B)
+{
+     return std::abs(A-B);
+}
+
+/**
+ * @brief Function to compare errors to a given tolerance, and decide if it's within range
+ *
+ * @param A The first value to compare
+ * @param B The second value to compare
+ * @param relative_tolerance The relative tolerance to use when comparing
+ *
+ * @return A pair containing true/false if it's within tolerance, and the calculated error
+ */
+std::pair<bool, double> compare_error(double A, double B, double relative_tolerance)
+{
+    bool within_tol = false;
+    double err = 0.0;
+
+    // Right now this is pretty arbitrary..
+    double abs_threshhold = 10 * std::numeric_limits<float>::epsilon();
+
+    // Calculate if we're withing tolerances
+    // If we're close to relative, do absolute
+    if (std::abs(std::min(A,B)) < abs_threshhold)
+    {
+        err = calculate_relative_error(A, B);
+
+        // Finding a relative error to 0 doesn't make much
+        // sense, so lets do absolute error instead
+        if ( err < std::numeric_limits<double>::epsilon() )
+        {
+            within_tol = true;
+        }
+        else {
+            within_tol = false;
+        }
+    }
+    else { // Do absolute error
+
+        err = calculate_abs_error(A, B);
+
+        if (err < relative_tolerance)
+        {
+            within_tol = true;
+        }
+        else {
+            within_tol = false;
+        }
+    }
+    return { within_tol, err };
+}
+
+
+/**
  * @brief Function to compare the contents of two energy files
  *
  * @param file_a First file to compare
  * @param file_b Second file to compare
  * @param relative_tolerance Relative tolerance which is acceptable
+ * @param field_mask A mask to specify which fields in the file to use
+ * @param sum_mask A mask to specify which fields in the file to sum and compare
  * @param num_lines_to_skip
  * @param write_err_ouput
  *
  * @return True is they match (within tol), false if not
  */
 bool compare_energies(
-        std::string file_a,
-        std::string file_b,
-        double relative_tolerance,
-        int num_lines_to_skip = 0, // Most energy files have 3 lines of padding
-        int write_err_ouput = 0 // If the run should dump the errors to disk
+        const std::string file_a,
+        const std::string file_b,
+        const double relative_tolerance,
+        const unsigned short field_mask = 0b1111111111111111, /// short has 16 bytes, assume all are true
+        const int num_lines_to_skip = 0, // Most energy files have 3 lines of padding
+        const int write_err_ouput = 0 // If the run should dump the errors to disk
 )
 {
     std::vector<double> errs;
@@ -59,9 +141,6 @@ bool compare_energies(
 
         std::ifstream f1 (file_a);
         std::ifstream f2 (file_b);
-
-        // Right now this is pretty arbitrary..
-        double abs_threshhold = 10 * std::numeric_limits<float>::epsilon();
 
         double max_err = 0.0;
         int max_err_line = -1;
@@ -97,55 +176,28 @@ bool compare_energies(
                 int this_line_token_count = 0;
                 while (getline(linestream1, item1, ' '))
                 {
-                    double err = 0.0;
-
                     this_line_token_count++;
                     getline(linestream2, item2, ' ');
 
                     double A = std::stod(item1);
                     double B = std::stod(item2);
 
-                    bool within_tol = false;
+                    std::pair<bool, double> returned_err = compare_error(A, B,
+                            relative_tolerance);
 
-                    // Calculate if we're withing tolerances
-                    // If we're close to relative, do absolute
-                    if (std::abs(std::min(A,B)) < abs_threshhold)
-                    {
-                        err = std::abs(A-B);
+                    bool returned_match = returned_err.first;
 
-                        // Finding a relative error to 0 doesn't make much
-                        // sense, so lets do absolute error instead
-                        if ( err < std::numeric_limits<double>::epsilon() )
-                        {
-                            within_tol = true;
-                        }
-                        else {
-                            within_tol = false;
-                        }
+                    if (!returned_match) {
+                        match = false;
                     }
-                    else { // Do absolute error
 
-                        err = std::abs(A-B) / std::min(A,B);
+                    double err = returned_err.second;
 
-
-                        // Track max absolute error
-                        if (err > max_err)
-                        {
-                            max_err = err;
-                            max_err_line = counter;
-                        }
-
-                        if (err < relative_tolerance)
-                        {
-                            within_tol = true;
-                        }
-                        else {
-                            within_tol = false;
-                        }
-
-                        if (! within_tol ) {
-                            match = false;
-                        }
+                    // Track max absolute error
+                    if (err > max_err)
+                    {
+                        max_err = err;
+                        max_err_line = counter;
                     }
 
                     // If we track the errors, track this one
@@ -162,7 +214,7 @@ bool compare_energies(
             f2.close();
         }
         else {
-            std::cout << "Unable to open file";
+            std::cerr << "Unable to open file";
             return false;
         }
 

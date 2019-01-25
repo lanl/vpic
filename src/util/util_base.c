64b4fa22d3603fdca0a6ccd14cc0df2301369401
Robert Bird <bird@lanl.gov>
2019-01-25 09:21:01 -0700

    removed unused variable

--- src/util/util_base.c
+++ src/util/util_base.c
@@ -58,9 +58,6 @@ int string_starts_with(const char *str, const char *pre)
 
 int string_contains(const char *str, const char *substr)
 {
-    const char *output = NULL;
-    output = strstr(str,substr);
-
     const char* pos = strstr(str, substr);
 
     if (pos) {

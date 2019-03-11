67b0fa19b2283c3f9052a35e1f121ffa16f3d75a
Dave Nystrom <wdn@lanl.gov>
2019-03-11 15:46:25 -0600

    Switch all C files to C++ files unless it is explicit that they need to be C files.

--- src/sf_interface/hydro_array.cc
+++ src/sf_interface/hydro_array.cc
@@ -1,4 +1,4 @@
-/*
+/* 
  * Written by:
  *   Kevin J. Bowers, Ph.D.
  *   Plasma Physics Group (X-1)
@@ -10,148 +10,75 @@
 
 #include "sf_interface.h"
 
-static int
-ha_n_pipeline( void )
-{
-#if defined(VPIC_USE_PTHREADS)                         // Pthreads case.
-  int                          n = serial.n_pipeline;
-  if ( n < thread.n_pipeline ) n = thread.n_pipeline;
-
-#elif defined(VPIC_USE_OPENMP)                         // OpenMP case.
-  int                          n = omp_helper.n_pipeline;
-
-#else                                                  // Error case.
-  #error "VPIC_USE_OPENMP or VPIC_USE_PTHREADS must be specified"
-
-#endif
-
-  return n; // max( {serial,thread}.n_pipeline )
-}
-
-// Though the checkpt / restore functions are not part of the public
-// API, they must not be declared as static.
+/* Though the checkpt/restore functions are not part of the public
+   API, they must not be declared as static. */
 
 void
-checkpt_hydro_array( const hydro_array_t * ha )
-{
+checkpt_hydro_array( const hydro_array_t * ha ) {
   CHECKPT( ha, 1 );
-
-  CHECKPT_ALIGNED( ha->h,
-                   (size_t) ( ha->n_pipeline + 1 ) * (size_t) ha->stride,
-                   128 );
-
+  CHECKPT_ALIGNED( ha->h, ha->g->nv, 128 );
   CHECKPT_PTR( ha->g );
 }
 
 hydro_array_t *
-restore_hydro_array( void )
-{
+restore_hydro_array( void ) {
   hydro_array_t * ha;
-
   RESTORE( ha );
-
   RESTORE_ALIGNED( ha->h );
-
   RESTORE_PTR( ha->g );
-
-  if ( ha->n_pipeline != ha_n_pipeline() )
-  {
-    ERROR( ( "Number of pipelines restored is not the same as the number of "
-             "pipelines checkpointed.  Did you change the number of threads "
-             "per process between checkpt and restore?" ) );
-  }
-
   return ha;
 }
 
 hydro_array_t *
-new_hydro_array( grid_t * g )
-{
+new_hydro_array( grid_t * g ) {
   hydro_array_t * ha;
-
-  if ( ! g )
-  {
-    ERROR( ( "NULL grid." ) );
-  }
-
+  if( !g ) ERROR(( "NULL grid" ));
   MALLOC( ha, 1 );
-
-  ha->n_pipeline = ha_n_pipeline();
-  ha->stride     = POW2_CEIL( g->nv, 2 );
-  ha->g          = g;
-
-  MALLOC_ALIGNED( ha->h,
-                  (size_t) ( ha->n_pipeline + 1 ) * (size_t) ha->stride,
-                  128 );
-
-  CLEAR( ha->h,
-         (size_t) ( ha->n_pipeline + 1 ) * (size_t) ha->stride );
-
-  REGISTER_OBJECT( ha,
-                   checkpt_hydro_array,
-                   restore_hydro_array,
-                   NULL );
-
+  MALLOC_ALIGNED( ha->h, g->nv, 128 );
+  ha->g = g;
+  clear_hydro_array( ha );
+  REGISTER_OBJECT( ha, checkpt_hydro_array, restore_hydro_array, NULL );
   return ha;
 }
 
 void
-delete_hydro_array( hydro_array_t * ha )
-{
-  if ( ! ha )
-  {
-    return;
-  }
-
+delete_hydro_array( hydro_array_t * ha ) {
+  if( !ha ) return;
   UNREGISTER_OBJECT( ha );
-
   FREE_ALIGNED( ha->h );
-
   FREE( ha );
 }
 
-#define hydro( x, y, z ) h0[ VOXEL( x, y, z, nx, ny, nz ) ]
+void
+clear_hydro_array( hydro_array_t * ha ) {
+  if( !ha ) ERROR(( "NULL hydro array" ));
+  CLEAR( ha->h, ha->g->nv ); // FIXME: SPU THIS?
+}
 
-// Generic looping.
+#define hydro(x,y,z) h0[ VOXEL(x,y,z, nx,ny,nz) ]
 
-#define XYZ_LOOP( xl, xh, yl, yh, zl, zh ) \
-  for( z = zl; z <= zh; z++ )              \
-    for( y = yl; y <= yh; y++ )            \
-      for( x = xl; x <= xh; x++ )
-
-// x_NODE_LOOP => Loop over all non-ghost nodes at plane x.
-
-#define x_NODE_LOOP( x ) XYZ_LOOP( x, x, 1, ny+1, 1, nz+1 )
-
-#define y_NODE_LOOP( y ) XYZ_LOOP( 1, nx+1, y, y, 1, nz+1 )
-
-#define z_NODE_LOOP( z ) XYZ_LOOP( 1, nx+1, 1, ny+1, z, z )
+// Generic looping
+#define XYZ_LOOP(xl,xh,yl,yh,zl,zh) \
+  for( z=zl; z<=zh; z++ )	    \
+    for( y=yl; y<=yh; y++ )	    \
+      for( x=xl; x<=xh; x++ )
+	      
+// x_NODE_LOOP => Loop over all non-ghost nodes at plane x
+#define x_NODE_LOOP(x) XYZ_LOOP(x,x,1,ny+1,1,nz+1)
+#define y_NODE_LOOP(y) XYZ_LOOP(1,nx+1,y,y,1,nz+1)
+#define z_NODE_LOOP(z) XYZ_LOOP(1,nx+1,1,ny+1,z,z)
 
 void
-synchronize_hydro_array( hydro_array_t * ha )
-{
+synchronize_hydro_array( hydro_array_t * ha ) {
   int size, face, bc, x, y, z, nx, ny, nz;
-
   float *p, lw, rw;
+  hydro_t * h0, * h;
+  grid_t * g;
 
-  hydro_t *h0, *h;
-
-  grid_t *g;
-
-  if ( ! ha )
-  {
-    ERROR( ( "NULL hydro array." ) );
-  }
-
-  // First reduce the pipelines.
-
-  reduce_hydro_array( ha );
-
-  // Now begin to synchronize.
+  if( !ha ) ERROR(( "NULL hydro array" ));
 
   h0 = ha->h;
   g  = ha->g;
-
   nx = g->nx;
   ny = g->ny;
   nz = g->nz;
@@ -159,18 +86,15 @@ synchronize_hydro_array( hydro_array_t * ha )
   // Note: synchronize_hydro assumes that hydro has not been adjusted
   // at the local domain boundary. Because hydro fields are purely
   // diagnostic, correct the hydro along local boundaries to account
-  // for accumulations over partial cell volumes.
+  // for accumulations over partial cell volumes
 
-  #define ADJUST_HYDRO( i, j, k, X, Y, Z )      \
-  do                                            \
-  {                                             \
-    bc = g->bc[ BOUNDARY( i, j, k ) ];          \
-    if ( bc < 0 || bc >= world_size )           \
-    {                                           \
-      face = ( i + j + k ) < 0 ? 1 : n##X + 1;  \
-      X##_NODE_LOOP( face )                     \
-      {                                         \
-        h       = &hydro( x, y, z );            \
+# define ADJUST_HYDRO(i,j,k,X,Y,Z)              \
+  do {                                          \
+    bc = g->bc[BOUNDARY(i,j,k)];                \
+    if( bc<0 || bc>=world_size ) {              \
+      face = (i+j+k)<0 ? 1 : n##X+1;            \
+      X##_NODE_LOOP(face) {                     \
+        h = &hydro(x,y,z);                      \
         h->jx  *= 2;                            \
         h->jy  *= 2;                            \
         h->jz  *= 2;                            \
@@ -187,125 +111,112 @@ synchronize_hydro_array( hydro_array_t * ha )
         h->txy *= 2;                            \
       }                                         \
     }                                           \
-  } while( 0 )
+  } while(0)
+  
+  ADJUST_HYDRO(-1, 0, 0,x,y,z);
+  ADJUST_HYDRO( 0,-1, 0,y,z,x);
+  ADJUST_HYDRO( 0, 0,-1,z,x,y);
+  ADJUST_HYDRO( 1, 0, 0,x,y,z);
+  ADJUST_HYDRO( 0, 1, 0,y,z,x);
+  ADJUST_HYDRO( 0, 0, 1,z,x,y);
 
-  ADJUST_HYDRO( (-1),   0,    0,  x, y, z );
-  ADJUST_HYDRO(   0,  (-1),   0,  y, z, x );
-  ADJUST_HYDRO(   0,    0,  (-1), z, x, y );
-  ADJUST_HYDRO(   1,    0,    0,  x, y, z );
-  ADJUST_HYDRO(   0,    1,    0,  y, z, x );
-  ADJUST_HYDRO(   0,    0,    1,  z, x, y );
+# undef ADJUST_HYDRO
 
-  #undef ADJUST_HYDRO
+# define BEGIN_RECV(i,j,k,X,Y,Z) \
+  begin_recv_port(i,j,k,( 1 + 14*(n##Y+1)*(n##Z+1) )*sizeof(float),g)
 
-  #define BEGIN_RECV( i, j, k, X, Y, Z ) \
-  begin_recv_port( i, j, k, ( 1 + 14 * ( n##Y + 1 ) * ( n##Z + 1 ) ) * sizeof(float), g )
-
-  #define BEGIN_SEND( i, j, k, X, Y, Z )                     \
-  BEGIN_PRIMITIVE                                            \
-  {                                                          \
-    size = ( 1 + 14 * (n##Y+1) * (n##Z+1) ) * sizeof(float); \
-    p = (float *) size_send_port( i, j, k, size, g );        \
-    if ( p )                                                 \
-    {                                                        \
-      ( *(p++) ) = g->d##X;                                  \
-      face       = ( i + j + k ) < 0 ? 1 : n##X+1;           \
-      X##_NODE_LOOP( face )                                  \
-      {                                                      \
-        h          = &hydro( x, y, z );                      \
-        ( *(p++) ) = h->jx;                                  \
-        ( *(p++) ) = h->jy;                                  \
-        ( *(p++) ) = h->jz;                                  \
-        ( *(p++) ) = h->rho;                                 \
-        ( *(p++) ) = h->px;                                  \
-        ( *(p++) ) = h->py;                                  \
-        ( *(p++) ) = h->pz;                                  \
-        ( *(p++) ) = h->ke;                                  \
-        ( *(p++) ) = h->txx;                                 \
-        ( *(p++) ) = h->tyy;                                 \
-        ( *(p++) ) = h->tzz;                                 \
-        ( *(p++) ) = h->tyz;                                 \
-        ( *(p++) ) = h->tzx;                                 \
-        ( *(p++) ) = h->txy;                                 \
-      }                                                      \
-      begin_send_port( i, j, k, size, g );                   \
-    }                                                        \
+# define BEGIN_SEND(i,j,k,X,Y,Z) BEGIN_PRIMITIVE {      \
+    size = ( 1 + 14*(n##Y+1)*(n##Z+1) )*sizeof(float);  \
+    p = (float *)size_send_port( i, j, k, size, g );    \
+    if( p ) {                                           \
+      (*(p++)) = g->d##X;                               \
+      face = (i+j+k)<0 ? 1 : n##X+1;                    \
+      X##_NODE_LOOP(face) {                             \
+        h = &hydro(x,y,z);                              \
+        (*(p++)) = h->jx;                               \
+        (*(p++)) = h->jy;                               \
+        (*(p++)) = h->jz;                               \
+        (*(p++)) = h->rho;                              \
+        (*(p++)) = h->px;                               \
+        (*(p++)) = h->py;                               \
+        (*(p++)) = h->pz;                               \
+        (*(p++)) = h->ke;                               \
+        (*(p++)) = h->txx;                              \
+        (*(p++)) = h->tyy;                              \
+        (*(p++)) = h->tzz;                              \
+        (*(p++)) = h->tyz;                              \
+        (*(p++)) = h->tzx;                              \
+        (*(p++)) = h->txy;                              \
+      }                                                 \
+      begin_send_port( i, j, k, size, g );              \
+    }                                                   \
   } END_PRIMITIVE
 
-  #define END_RECV( i, j, k, X, Y, Z )                                 \
-  BEGIN_PRIMITIVE                                                      \
-  {                                                                    \
-    p = (float *) end_recv_port( i, j, k, g );                         \
-    if ( p )                                                           \
-    {                                                                  \
-      rw    = ( *(p++) );                     /* Remote g->d##X */     \
-      lw    = rw + g->d##X;                                            \
-      rw   /= lw;                                                      \
-      lw    = g->d##X / lw;                                            \
-      lw   += lw;                                                      \
-      rw   += rw;                                                      \
-      face  = ( i + j + k ) < 0 ? n##X+1 : 1; /* Twice weighted sum */ \
-      X##_NODE_LOOP( face )                                            \
-      {                                                                \
-        h      = &hydro( x, y, z );                                    \
-        h->jx  = lw * h->jx  + rw * ( *(p++) );                        \
-        h->jy  = lw * h->jy  + rw * ( *(p++) );                        \
-        h->jz  = lw * h->jz  + rw * ( *(p++) );                        \
-        h->rho = lw * h->rho + rw * ( *(p++) );                        \
-        h->px  = lw * h->px  + rw * ( *(p++) );                        \
-        h->py  = lw * h->py  + rw * ( *(p++) );                        \
-        h->pz  = lw * h->pz  + rw * ( *(p++) );                        \
-        h->ke  = lw * h->ke  + rw * ( *(p++) );                        \
-        h->txx = lw * h->txx + rw * ( *(p++) );                        \
-        h->tyy = lw * h->tyy + rw * ( *(p++) );                        \
-        h->tzz = lw * h->tzz + rw * ( *(p++) );                        \
-        h->tyz = lw * h->tyz + rw * ( *(p++) );                        \
-        h->tzx = lw * h->tzx + rw * ( *(p++) );                        \
-        h->txy = lw * h->txy + rw * ( *(p++) );                        \
-      }                                                                \
-    }                                                                  \
+# define END_RECV(i,j,k,X,Y,Z) BEGIN_PRIMITIVE {                \
+    p = (float *)end_recv_port(i,j,k,g);                        \
+    if( p ) {                                                   \
+      rw = (*(p++));                 /* Remote g->d##X */       \
+      lw = rw + g->d##X;                                        \
+      rw /= lw;                                                 \
+      lw = g->d##X/lw;                                          \
+      lw += lw;                                                 \
+      rw += rw;                                                 \
+      face = (i+j+k)<0 ? n##X+1 : 1; /* Twice weighted sum */   \
+      X##_NODE_LOOP(face) {                                     \
+        h = &hydro(x,y,z);                                      \
+        h->jx  = lw*h->jx  + rw*(*(p++));                       \
+        h->jy  = lw*h->jy  + rw*(*(p++));                       \
+        h->jz  = lw*h->jz  + rw*(*(p++));                       \
+        h->rho = lw*h->rho + rw*(*(p++));                       \
+        h->px  = lw*h->px  + rw*(*(p++));                       \
+        h->py  = lw*h->py  + rw*(*(p++));                       \
+        h->pz  = lw*h->pz  + rw*(*(p++));                       \
+        h->ke  = lw*h->ke  + rw*(*(p++));                       \
+        h->txx = lw*h->txx + rw*(*(p++));                       \
+        h->tyy = lw*h->tyy + rw*(*(p++));                       \
+        h->tzz = lw*h->tzz + rw*(*(p++));                       \
+        h->tyz = lw*h->tyz + rw*(*(p++));                       \
+        h->tzx = lw*h->tzx + rw*(*(p++));                       \
+        h->txy = lw*h->txy + rw*(*(p++));                       \
+      }                                                         \
+    }                                                           \
   } END_PRIMITIVE
 
-  #define END_SEND( i, j, k, X, Y, Z ) end_send_port( i, j, k, g )
+# define END_SEND(i,j,k,X,Y,Z) end_send_port( i, j, k, g )
 
-  // Exchange x-faces.
+  // Exchange x-faces
+  BEGIN_SEND(-1, 0, 0,x,y,z);
+  BEGIN_SEND( 1, 0, 0,x,y,z);
+  BEGIN_RECV(-1, 0, 0,x,y,z);
+  BEGIN_RECV( 1, 0, 0,x,y,z);
+  END_RECV(-1, 0, 0,x,y,z);
+  END_RECV( 1, 0, 0,x,y,z);
+  END_SEND(-1, 0, 0,x,y,z);
+  END_SEND( 1, 0, 0,x,y,z);
 
-  BEGIN_SEND( (-1), 0, 0, x, y, z );
-  BEGIN_SEND(   1,  0, 0, x, y, z );
-  BEGIN_RECV( (-1), 0, 0, x, y, z );
-  BEGIN_RECV(   1,  0, 0, x, y, z );
+  // Exchange y-faces
+  BEGIN_SEND( 0,-1, 0,y,z,x);
+  BEGIN_SEND( 0, 1, 0,y,z,x);
+  BEGIN_RECV( 0,-1, 0,y,z,x);
+  BEGIN_RECV( 0, 1, 0,y,z,x);
+  END_RECV( 0,-1, 0,y,z,x);
+  END_RECV( 0, 1, 0,y,z,x);
+  END_SEND( 0,-1, 0,y,z,x);
+  END_SEND( 0, 1, 0,y,z,x);
 
-  END_RECV( (-1), 0, 0, x, y, z );
-  END_RECV(   1,  0, 0, x, y, z );
-  END_SEND( (-1), 0, 0, x, y, z );
-  END_SEND(   1,  0, 0, x, y, z );
+  // Exchange z-faces
+  BEGIN_SEND( 0, 0,-1,z,x,y);
+  BEGIN_SEND( 0, 0, 1,z,x,y);
+  BEGIN_RECV( 0, 0,-1,z,x,y);
+  BEGIN_RECV( 0, 0, 1,z,x,y);
+  END_RECV( 0, 0,-1,z,x,y);
+  END_RECV( 0, 0, 1,z,x,y);
+  END_SEND( 0, 0,-1,z,x,y);
+  END_SEND( 0, 0, 1,z,x,y);
 
-  // Exchange y-faces.
-
-  BEGIN_SEND( 0, (-1), 0, y, z, x );
-  BEGIN_SEND( 0,   1,  0, y, z, x );
-  BEGIN_RECV( 0, (-1), 0, y, z, x );
-  BEGIN_RECV( 0,   1,  0, y, z, x );
-
-  END_RECV( 0, (-1), 0, y, z, x );
-  END_RECV( 0,   1,  0, y, z, x );
-  END_SEND( 0, (-1), 0, y, z, x );
-  END_SEND( 0,   1,  0, y, z, x );
-
-  // Exchange z-faces.
-
-  BEGIN_SEND( 0, 0, (-1), z, x, y );
-  BEGIN_SEND( 0, 0,   1,  z, x, y );
-  BEGIN_RECV( 0, 0, (-1), z, x, y );
-  BEGIN_RECV( 0, 0,   1,  z, x, y );
-
-  END_RECV( 0, 0, (-1), z, x, y );
-  END_RECV( 0, 0,   1,  z, x, y );
-  END_SEND( 0, 0, (-1), z, x, y );
-  END_SEND( 0, 0,   1,  z, x, y );
-
-  #undef BEGIN_RECV
-  #undef BEGIN_SEND
-  #undef END_RECV
-  #undef END_SEND
+# undef BEGIN_RECV
+# undef BEGIN_SEND
+# undef END_RECV
+# undef END_SEND
 }
+
--- src/species_advance/standard/hydro_p.cc
+++ src/species_advance/standard/hydro_p.cc
@@ -1,19 +1,167 @@
+// FIXME: THREAD THIS! HYDRO MEM SEMANTICS WILL NEED UPDATING.
+// FIXME: V4 ACCELERATE THIS.  COULD BE BASED OFF ENERGY_P.
+
+/* 
+ * Written by:
+ *   Kevin J. Bowers, Ph.D.
+ *   Plasma Physics Group (X-1)
+ *   Applied Physics Division
+ *   Los Alamos National Lab
+ * March/April 2004 - Revised and extended from earlier V4PIC versions
+ *
+ */
+
 #define IN_spa
 
 #include "../species_advance.h"
 
-//----------------------------------------------------------------------------//
-// Top level function to select and call particle hydro accumulation function
-// using the desired particle advance abstraction.  Currently, the only
-// abstraction available is the pipeline abstraction.
-//----------------------------------------------------------------------------//
+// accumulate_hydro_p adds the hydrodynamic fields associated with the
+// supplied particle_list to the hydro array.  Trilinear interpolation
+// is used.  hydro is known at the nodes at the same time as particle
+// positions. No effort is made to fix up edges of the computational
+// domain.  All particles on the list must be inbounds.  Note, the
+// hydro jx,jy,jz are for diagnostic purposes only; they are not
+// accumulated with a charge conserving algorithm.
 
 void
-accumulate_hydro_p( hydro_array_t * RESTRICT ha,
-                    const species_t * RESTRICT sp,
-                    const interpolator_array_t * RESTRICT ia )
-{
-  // Once more options are available, this should be conditionally executed
-  // based on user choice.
-  accumulate_hydro_p_pipeline( ha, sp, ia );
+accumulate_hydro_p( hydro_array_t              * RESTRICT ha,
+                    const species_t            * RESTRICT sp,
+                    const interpolator_array_t * RESTRICT ia ) {
+  /**/  hydro_t        * RESTRICT ALIGNED(128) h;
+  const particle_t     * RESTRICT ALIGNED(128) p;
+  const interpolator_t * RESTRICT ALIGNED(128) f;
+  float c, qsp, mspc, qdt_2mc, qdt_4mc2, r8V;
+  int np, stride_10, stride_21, stride_43;
+
+  float dx, dy, dz, ux, uy, uz, w, vx, vy, vz, ke_mc;
+  float w0, w1, w2, w3, w4, w5, w6, w7, t;
+  int i, n;
+
+  if( !ha || !sp || !ia || ha->g!=sp->g || ha->g!=ia->g )
+    ERROR(( "Bad args" ));
+
+  h = ha->h;
+  p = sp->p;
+  f = ia->i;
+
+  c        = sp->g->cvac;
+  qsp      = sp->q;
+  mspc     = sp->m*c;
+  qdt_2mc  = (qsp*sp->g->dt)/(2*mspc);
+  qdt_4mc2 = qdt_2mc / (2*c);
+  r8V      = sp->g->r8V;
+
+  np        = sp->np;
+  stride_10 = VOXEL(1,0,0, sp->g->nx,sp->g->ny,sp->g->nz) -
+              VOXEL(0,0,0, sp->g->nx,sp->g->ny,sp->g->nz);
+  stride_21 = VOXEL(0,1,0, sp->g->nx,sp->g->ny,sp->g->nz) -
+              VOXEL(1,0,0, sp->g->nx,sp->g->ny,sp->g->nz);
+  stride_43 = VOXEL(0,0,1, sp->g->nx,sp->g->ny,sp->g->nz) -
+              VOXEL(1,1,0, sp->g->nx,sp->g->ny,sp->g->nz);
+
+  for( n=0; n<np; n++ ) {
+
+    // Load the particle
+    dx = p[n].dx;
+    dy = p[n].dy;
+    dz = p[n].dz;
+    i  = p[n].i;
+    ux = p[n].ux;
+    uy = p[n].uy;
+    uz = p[n].uz;
+    w  = p[n].w;
+    
+    // Half advance E
+    ux += qdt_2mc*((f[i].ex+dy*f[i].dexdy) + dz*(f[i].dexdz+dy*f[i].d2exdydz));
+    uy += qdt_2mc*((f[i].ey+dz*f[i].deydz) + dx*(f[i].deydx+dz*f[i].d2eydzdx));
+    uz += qdt_2mc*((f[i].ez+dx*f[i].dezdx) + dy*(f[i].dezdy+dx*f[i].d2ezdxdy));
+
+    // Boris rotation - Interpolate B field
+    w5 = f[i].cbx + dx*f[i].dcbxdx;
+    w6 = f[i].cby + dy*f[i].dcbydy;
+    w7 = f[i].cbz + dz*f[i].dcbzdz;
+
+    // Boris rotation - curl scalars (0.5 in v0 for half rotate) and
+    // kinetic energy computation. Note: gamma-1 = |u|^2 / (gamma+1)
+    // is the numerically accurate way to compute gamma-1
+    ke_mc = ux*ux + uy*uy + uz*uz; // ke_mc = |u|^2 (invariant)
+    vz = sqrt(1+ke_mc);            // vz = gamma    (invariant)
+    ke_mc *= c/(vz+1);             // ke_mc = c|u|^2/(gamma+1) = c*(gamma-1)
+    vz = c/vz;                     // vz = c/gamma
+    w0 = qdt_4mc2*vz;
+    w1 = w5*w5 + w6*w6 + w7*w7;    // |cB|^2
+    w2 = w0*w0*w1;
+    w3 = w0*(1+(1./3.)*w2*(1+0.4*w2));
+    w4 = w3/(1 + w1*w3*w3); w4 += w4;
+
+    // Boris rotation - uprime
+    w0 = ux + w3*( uy*w7 - uz*w6 );
+    w1 = uy + w3*( uz*w5 - ux*w7 );
+    w2 = uz + w3*( ux*w6 - uy*w5 );
+
+    // Boris rotation - u
+    ux += w4*( w1*w7 - w2*w6 );
+    uy += w4*( w2*w5 - w0*w7 );
+    uz += w4*( w0*w6 - w1*w5 );
+
+    // Compute physical velocities
+    vx  = ux*vz;
+    vy  = uy*vz;
+    vz *= uz;
+
+    // Compute the trilinear coefficients
+    w0  = r8V*w;    // w0 = (1/8)(w/V)
+    dx *= w0;       // dx = (1/8)(w/V) x
+    w1  = w0+dx;    // w1 = (1/8)(w/V) + (1/8)(w/V)x = (1/8)(w/V)(1+x)
+    w0 -= dx;       // w0 = (1/8)(w/V) - (1/8)(w/V)x = (1/8)(w/V)(1-x)
+    w3  = 1+dy;     // w3 = 1+y
+    w2  = w0*w3;    // w2 = (1/8)(w/V)(1-x)(1+y)
+    w3 *= w1;       // w3 = (1/8)(w/V)(1+x)(1+y)
+    dy  = 1-dy;     // dy = 1-y
+    w0 *= dy;       // w0 = (1/8)(w/V)(1-x)(1-y)
+    w1 *= dy;       // w1 = (1/8)(w/V)(1+x)(1-y)
+    w7  = 1+dz;     // w7 = 1+z
+    w4  = w0*w7;    // w4 = (1/8)(w/V)(1-x)(1-y)(1+z) = (w/V) trilin_0 *Done
+    w5  = w1*w7;    // w5 = (1/8)(w/V)(1+x)(1-y)(1+z) = (w/V) trilin_1 *Done
+    w6  = w2*w7;    // w6 = (1/8)(w/V)(1-x)(1+y)(1+z) = (w/V) trilin_2 *Done
+    w7 *= w3;       // w7 = (1/8)(w/V)(1+x)(1+y)(1+z) = (w/V) trilin_3 *Done
+    dz  = 1-dz;     // dz = 1-z
+    w0 *= dz;       // w0 = (1/8)(w/V)(1-x)(1-y)(1-z) = (w/V) trilin_4 *Done
+    w1 *= dz;       // w1 = (1/8)(w/V)(1+x)(1-y)(1-z) = (w/V) trilin_5 *Done
+    w2 *= dz;       // w2 = (1/8)(w/V)(1-x)(1+y)(1-z) = (w/V) trilin_6 *Done
+    w3 *= dz;       // w3 = (1/8)(w/V)(1+x)(1+y)(1-z) = (w/V) trilin_7 *Done
+
+    // Accumulate the hydro fields
+#   define ACCUM_HYDRO( wn)                             \
+    t  = qsp*wn;        /* t  = (qsp w/V) trilin_n */   \
+    h[i].jx  += t*vx;                                   \
+    h[i].jy  += t*vy;                                   \
+    h[i].jz  += t*vz;                                   \
+    h[i].rho += t;                                      \
+    t  = mspc*wn;       /* t = (msp c w/V) trilin_n */  \
+    dx = t*ux;          /* dx = (px w/V) trilin_n */    \
+    dy = t*uy;                                          \
+    dz = t*uz;                                          \
+    h[i].px  += dx;                                     \
+    h[i].py  += dy;                                     \
+    h[i].pz  += dz;                                     \
+    h[i].ke  += t*ke_mc;                                \
+    h[i].txx += dx*vx;                                  \
+    h[i].tyy += dy*vy;                                  \
+    h[i].tzz += dz*vz;                                  \
+    h[i].tyz += dy*vz;                                  \
+    h[i].tzx += dz*vx;                                  \
+    h[i].txy += dx*vy
+
+    /**/            ACCUM_HYDRO(w0); // Cell i,j,k
+    i += stride_10; ACCUM_HYDRO(w1); // Cell i+1,j,k
+    i += stride_21; ACCUM_HYDRO(w2); // Cell i,j+1,k
+    i += stride_10; ACCUM_HYDRO(w3); // Cell i+1,j+1,k
+    i += stride_43; ACCUM_HYDRO(w4); // Cell i,j,k+1
+    i += stride_10; ACCUM_HYDRO(w5); // Cell i+1,j,k+1
+    i += stride_21; ACCUM_HYDRO(w6); // Cell i,j+1,k+1
+    i += stride_10; ACCUM_HYDRO(w7); // Cell i+1,j+1,k+1
+
+#   undef ACCUM_HYDRO
+  }
 }

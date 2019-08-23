3392b4865987fe10303ccc82a42864d989c0405c
Robert Bird <bird@lanl.gov>
2019-08-23 09:20:14 -0600

    enable checkpointing of particle id

--- src/species_advance/species_advance.cc
+++ src/species_advance/species_advance.cc
@@ -18,8 +18,14 @@ checkpt_species( const species_t * sp )
   CHECKPT( sp, 1 );
   CHECKPT_STR( sp->name );
   checkpt_data( sp->p,
-                sp->np    *sizeof(particle_t),
-                sp->max_np*sizeof(particle_t), 1, 1, 128 );
+                sp->np     * sizeof(particle_t),
+                sp->max_np * sizeof(particle_t), 1, 1, 128 );
+
+  // RFB: Add checkpointing of particle list
+  checkpt_data( sp->p_id,
+                sp->np    *sizeof(size_t),
+                sp->max_np*sizeof(size_t), 1, 1, 128 );
+
   checkpt_data( sp->pm,
                 sp->nm    *sizeof(particle_mover_t),
                 sp->max_nm*sizeof(particle_mover_t), 1, 1, 128 );
@@ -34,8 +40,9 @@ restore_species( void )
   species_t * sp;
   RESTORE( sp );
   RESTORE_STR( sp->name );
-  sp->p  = (particle_t *)      restore_data();
-  sp->pm = (particle_mover_t *)restore_data();
+  sp->p  = (particle_t *)        restore_data();
+  sp->p_id  = (size_t*)          restore_data();
+  sp->pm = (particle_mover_t *)  restore_data();
   RESTORE_ALIGNED( sp->partition );
   RESTORE_PTR( sp->g );
   RESTORE_PTR( sp->next );
@@ -49,6 +56,7 @@ delete_species( species_t * sp )
   FREE_ALIGNED( sp->partition );
   FREE_ALIGNED( sp->pm );
   FREE_ALIGNED( sp->p );
+  FREE_ALIGNED( sp->p_id );
   FREE( sp->name );
   FREE( sp );
 }
